002b06223c75558a120eba879f8fda7b05b2bb55
student <student@comp04.class218>
2019-04-02 13:37:26 +0400

    Obnovleniye Brado

--- Brado.c
+++ Brado.c
@@ -3,3 +3,29 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <semaphore.h>
+
+sem_t sem;
+sem_t divan;
+pthread_mutex_t brado;
+
+
+int main(void) {
+	int i, res;
+	pthread_t ;
+	sem_init(&sem,0,0);
+	sem_init(&divan,0,0);
+	pthread_mutex_init(&brado,0);
+	return EXIT_SUCCESS;
+}
+
+void *bradobrey(void *arg) {
+	int loc_id = *(int *)arg;
+	
+	return NULL;
+}
+
+void *posetitel(void *arg) {
+	int loc_id = *(int *)arg;
+	
+	return NULL;
+}
