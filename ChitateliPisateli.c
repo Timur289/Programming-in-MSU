0a92ab5b33dbc256602ece6a81050aaac935818b
student <student@comp04.class218>
2019-04-12 15:37:26 +0400

    Zaebalsa

--- ChitateliPisateli.c
+++ ChitateliPisateli.c
@@ -6,9 +6,9 @@
 #define N 40
 #define M 10
 sem_t sem;
-sem_t bibliotekorsha;
+sem_t bibl;
 sem_t database;
-sem_t cherniyvxod;
+sem_t pis;
 pthread_mutex_t sost;
 
 int sostbibl = 0;
@@ -19,14 +19,12 @@ void *chitatel(void *);
 void *pisatel(void *);
 
 int main(void){
-  printf("Smth");
   int i, res;
   pthread_t ch[N], ps[M];
   sem_init(&sem,0,0);
-  sem_init(&bibliotekorsha,0,1);
+  sem_init(&bibl,0,0);
   sem_init(&database,0,1);
-  sem_init(&cherniyvxod,0,1);
-  printf("sldg");
+  sem_init(&pis,0,1);
   for(i = 0; i < N; i++){
     res = pthread_create(&ch[i], NULL, chitatel, &i);
     if(res) return EXIT_FAILURE;
@@ -54,9 +52,9 @@ int main(void){
   }
 
   sem_destroy(&sem);
-  sem_destroy(&bibliotekorsha);
+  sem_destroy(&bibl);
   sem_destroy(&database);
-  sem_destroy(&cherniyvxod);
+  sem_destroy(&pis);
   pthread_mutex_destroy(&sost);
 
   printf("Done\n\n");
@@ -67,19 +65,23 @@ int main(void){
 void *chitatel(void *arg){
   int id = *(int *)arg;
   int i;
-  sem_post(&sem);//запускаем по очереди
+  sem_post(&sem);
   for(i = 0; i < 3; i++) {
-	 
-    sem_wait(&bibliotekorsha);//просим библиотекоршу оформить пропуск
-    n++;
-    sem_post(&bibliotekorsha); //библиотекорша дает читателю пропуск и ждет следующего посетителя
-    printf("Reader %d is reading\n\n", id + 1);
-    sleep(2);
-    sem_wait(&bibliotekorsha); //выходим и оформляемся у библиотекорши
-    n--;
-    sem_post(&bibliotekorsha); //уходим и отпускаем библиотекоршу
-    if(n == 0)  sem_post(&database); //последний читатель ушел и открыл библиотеку писателям
-    //sleep(15);
+	pthread_mutex_lock(&sost);
+	if(sostbibl >= 0) {
+		sostbibl++;
+		pthread_mutex_unlock(&sost);
+		printf("Reader %d is reading\n\n", id + 1);
+		sleep(2);
+		pthread_mutex_lock(&sost);
+		sostbibl--;
+		pthread_mutex_unlock(&sost);
+		if(!sostbibl) sem_post(&bibl);
+	}
+	else {
+		printf("Pisatel zanal biblioteku i chitatel jdet\n\n");
+		pthread_mutex_unlock(&sost);
+	}
   }
   return NULL;
 }
@@ -88,18 +90,25 @@ void *pisatel(void *arg) {
   int id = *(int *)arg;
   int i;
   sem_post(&sem);
-  
+  pthread_mutex_lock(&sost);
   for(i = 0; i < 2; i++){
-    sem_wait(&cherniyvxod);//просим библиотекоршу оформить пропуск
-    m++;
-    if(m == 1) sem_wait(&database); //первый писатель присваевает библиотеку писателям
-    sem_post(&cherniyvxod); //библиотекорша дает писателю пропуск и ждет следующего посетителя
-    printf("Writer %d is writing\n\n", id + 1);
-    sleep(6);
-    sem_wait(&cherniyvxod); //выходим к черному входу
-    m--;
-    sem_post(&cherniyvxod); //уходим через черный вход
-    if(m == 0)  sem_post(&database);
+	if(!sostbibl) {
+		sostbibl--;
+		pthread_mutex_unlock(&sost);
+		sem_wait(&pis);
+		printf("Writer %d is writing\n\n", id + 1);
+		sleep(6);
+		pthread_mutex_lock(&sost);
+		sostbibl = 0;
+		pthread_mutex_unlock(&sost);
+		sem_post(&pis);
+	}
+	else {
+		printf("Pisatel ne mojet zayti v bibl\n\n");
+		pthread_mutex_unlock(&sost);
+		sem_wait(&bibl);
+		sem_post(&bibl);
+	}
   }
   return NULL;
 }
